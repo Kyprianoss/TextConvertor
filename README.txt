36b6dd2db8839caec3c6cb459b2801b7a985b549
Kyprianoss <54744733+Kyprianoss@users.noreply.github.com>
2021-03-17 10:29:42 +0200

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,8 @@
+BinConvert
+It converts text to binary numbers. Features:
+- Option to select convert method (text to binary, text to ASCII)
+- Option to save console output to a text file
+
+Note: Placing a space in text input will cause errors so instead of space place "_"
+
+If your antivirus is blocking this program convert the *.cpp file to exe and then run it
---
+++ TextConvertor.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <bitset>
+#include <fstream>
+#include <windows.h>
+
+using namespace std;
+
+string content;
+char yORn;
+char options;
+string plainText;
+
+void exitconsole() {
+    char exitcons;
+    cout<<endl<<endl<<"You can now close this window"<<endl;
+    cin>>exitcons;
+}
+
+void convertToASCII(string letter)
+{
+    for (int i = 0; i < letter.length(); i++)
+    {
+        char x = letter.at(i);
+        cout <<int(x)<<endl;
+    }
+    cout<<endl<<"Do you want to store this in a text file? [y/n]"<<endl;
+    cin>>yORn;
+    if (yORn == 'y') {
+        ofstream makefile("converted.txt");
+    for (int i = 0; i < letter.length(); i++)
+    {
+        char x = letter.at(i);
+        makefile<<int(x)<<" ";
+    }
+        char filename[] = "converted.txt";
+        char fullFilename[MAX_PATH];
+        GetFullPathName(filename, MAX_PATH, fullFilename, nullptr);
+        cout<<"Converted.txt file has been created in "<<fullFilename<<endl;
+        exitconsole();
+    }
+    else if (yORn == 'n') {
+        cout<<"No selected";
+        exitconsole();
+    }
+    else {
+        cout<<options<<" is not an option"<<endl;
+        exitconsole();
+    }
+}
+
+
+int main()
+{   cout<<"TextConvertor"<<"Choose what you want to do:"<<endl<<"Text to binary: b"<<endl<<"Text to ASCII: a"<<endl<<"[b/a]: ";
+    cin>>options;
+    if (options == 'b') {
+    cout<<"Enter text to be converted to binary (placing a space in the text will cause errors):"<<endl;
+    cin>>content;
+      for (std::size_t i = 0; i <  content.size(); ++i) {
+        cout << bitset<8>( content.c_str()[i]) <<endl;
+    }
+        cout<<endl<<"Do you want to store this in a text file? [y/n]"<<endl;
+        cin>>yORn;
+        if (yORn == 'y') {
+            ofstream makefile("converted.txt");
+            for (std::size_t i = 0; i <  content.size(); ++i) {
+                makefile << bitset<8>( content.c_str()[i]) <<" ";
+            }
+            char filename[] = "converted.txt";
+            char fullFilename[MAX_PATH];
+            GetFullPathName(filename, MAX_PATH, fullFilename, nullptr);
+            cout<<"Converted.txt file has been created in "<<fullFilename<<endl;
+            exitconsole();
+        }
+       else if (yORn == 'n') {
+            cout<<"No selected";
+            exitconsole();
+        }
+       else {
+        cout<<yORn<<" is not y or n";
+        exitconsole();
+       }
+    }
+    else if (options == 'a') {
+        cout << "Enter text to be converted to ASCII (placing a space in the text will cause errors):"<<endl;
+        cin >> plainText;
+        convertToASCII(plainText);
+        }
+    else {
+        cout<<options<<" is not an option"<<endl;
+    }
+    return 0;
+}
